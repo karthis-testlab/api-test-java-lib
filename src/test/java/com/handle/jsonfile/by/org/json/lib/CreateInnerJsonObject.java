f6d2fc88f17ecdd4fbd92ad116d5addb2055622d
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-01-17 23:20:09 +0530

    17/01/2025:11:20:00PM

--- src/test/java/com/handle/jsonfile/by/org/json/lib/CreateInnerJsonObject.java
+++ src/test/java/com/handle/jsonfile/by/org/json/lib/CreateInnerJsonObject.java
@@ -2,8 +2,7 @@ package com.handle.jsonfile.by.org.json.lib;
 
 public class CreateInnerJsonObject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 
 	}
 
