4a417c3a69f58fead3ed54d4b8c4b6d887e54ef1
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-01-18 00:25:04 +0530

    18/01/2025:12:24:00AM

--- src/test/java/com/handle/jsonfile/by/org/json/lib/CreateInnerJsonObject.java
+++ src/test/java/com/handle/jsonfile/by/org/json/lib/CreateInnerJsonObject.java
@@ -1,8 +1,12 @@
 package com.handle.jsonfile.by.org.json.lib;
 
+import org.json.JSONObject;
+
 public class CreateInnerJsonObject {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		
+		JSONObject parent = new JSONObject();  
 
 	}
 
